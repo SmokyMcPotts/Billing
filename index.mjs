595ae5f7bd549267ad6ecae33d226c826b7377af
SmokyMcPotts <108499060+SmokyMcPotts@users.noreply.github.com>
2022-07-19 16:36:46 -0400

    Add files via upload
    Current error
    file:///app/build/index.main.mjs:190
      stdlib.protect(ctc0, await interact.reportCompletion(v97), {
                                          ^
    
    TypeError: interact.reportCompletion is not a function
        at Module.Alice (file:///app/build/index.main.mjs:190:39)
        at processTicksAndRejections (node:internal/process/task_queues:96:5)
        at async Promise.all (index 0)
        at async file:///app/index.mjs:27:1
    ERROR: 1

--- index.mjs
+++ index.mjs
@@ -3,12 +3,7 @@ import * as backend from './build/index.main.mjs';
 import {ask} from '@reach-sh/stdlib';
 const stdlib = loadStdlib(process.env);
 
-if (process.argv.length < 3 || ['Alice', 'Bob'].includes(process.argv[2]) == false) {
-  console.log('Usage: reach run index [Alice|Bob]');
-  process.exit(0);
-}
-const userName = process.argv[2];
-console.log(`Hello ${userName}`);
+console.log(`Hello Alice`);
 
 const startingBalance = stdlib.parseCurrency(100);
 
@@ -21,30 +16,31 @@ const ctcAlice = accAlice.contract(backend);
 const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
 
 console.log('Starting backends...');
-const sharedFunctions = (who) => ({
-  reportCompletion: () => {
-    console.log(`${(userName == 'Bob') ? 'You have' : 'Bob has'} been allowed into the shuffle.`)
-  },
-  reportMismatch: () => {
-    console.log(`${(userName == 'Bob') ? 'You are not able to join this shuffle. \nPlease contact the creator' : 'Invalid access attempt made'}.`)
+const sharedFunctions = (Who) => ({
+  reportCompletion: (result) => {
+    const close = result;
+    console.log(`${(close) ? 'Bob has been allowed into the shuffle' : 'Unauthorized entry attempted'}.`);
+    return close;
   },
 });
+console.log('Shared Functions loaded...')
 await Promise.all([
   backend.Alice(ctcAlice, {
-    ...stdlib.hasRandom,
     ...sharedFunctions,
-    whitelist : await ask.ask(`Your address is ${accAlice.getAddress()}. \nPlease enter the address you would like to add to the whitelist. \nPress enter to add ${accBob.getAddress()}.`, (addr) => {
+    addressAlice : accAlice.getAddress(),  
+    whitelist : await ask.ask(`Please enter the address you would like to add to the whitelist. \nPress enter to add ${accBob.getAddress()}.`, (addr) => {
       let ans = !addr ? accBob.getAddress() : addr;
       return ans;
     }),
     // implement Alice's interact object here
   }),
   backend.Bob(ctcBob, {
-    addressBob : accBob.getAddress(),
-    ...stdlib.hasRandom,
     ...sharedFunctions,
+    addressBob : accBob.getAddress(),
     // implement Bob's interact object here
   }),
 ]);
 
 console.log('Goodbye, Alice and Bob!');
+
+
--- index.rsh
+++ index.rsh
@@ -1,42 +1,48 @@
 'reach 0.1';
 
 const sharedFunctions = {
-  reportCompletion : Fun([], Null),
-  reportMismatch: Fun([], Null),
+  reportCompletion : Fun([Bool], Null),
 };
+
+const aliceInteract = {
+  ...sharedFunctions,
+  addressAlice : Address,
+  whitelist : Address,
+};
+
+const bobInteract = {
+  ...sharedFunctions,
+  addressBob : Address,
+}
 
 export const main = Reach.App(() => {
   setOptions({ untrustworthyMaps: true });
-  const A = Participant('Alice', {
-    whitelist : Address,
-    ...sharedFunctions,
+  const A = Participant('Alice', aliceInteract);
     // Specify Alice's interact interface here
-  });
-  const B = Participant('Bob', {
-    addressBob : Address,
-    ...sharedFunctions,
+  const B = Participant('Bob', bobInteract);
     // Specify Bob's interact interface here
-  });
   init();
   // The first one to publish deploys the contract
+  A.only(() => { const addressAlice = declassify(interact.addressAlice); 
+  });
+  A.publish(addressAlice);
+  commit();
+
+  B.only(() => { const addressBob = declassify(interact.addressBob);
+  });
+  B.publish(addressBob);
+  commit();
+
   A.only(() => { const whitelist = declassify(interact.whitelist);  
   });
   A.publish(whitelist);
   const compareSet = new Set();
   compareSet.insert(whitelist);
+  const result = (compareSet.member(addressBob));
+  each ([A, B], () => {
+    interact.reportCompletion(result); 
+  });
   commit();
-  // The second one to publish always attaches
-  B.only(() => {
-    const addressBob = declassify(interact.addressBob);
-  })
-  B.publish(addressBob);
-  // write your program here
-  if (compareSet.member(addressBob)) {
-    commit();
-    each ([A,B], () => interact.reportCompletion());
-  } else {
-    commit();
-    each([A,B], () => interact.reportMismatch());
-  };
+  
   exit();
 });
